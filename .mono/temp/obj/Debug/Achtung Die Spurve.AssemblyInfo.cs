4ccb539d7edf43411c502bbc2915ad979f93aeeb
jarlsoeren <69168989+jarlsoeren@users.noreply.github.com>
2026-07-07 23:24:50 +0200

    working trace

--- .mono/temp/obj/Debug/Achtung Die Spurve.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/Achtung Die Spurve.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Achtung Die Spurve")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1ef06b57596281a1bdecb11ebd1f13b18a0046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51d6723a74a42b612f34090430b62fc1baa02213")]
 [assembly: System.Reflection.AssemblyProductAttribute("Achtung Die Spurve")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Achtung Die Spurve")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
